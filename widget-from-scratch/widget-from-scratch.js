c1fa66c6f558750913b5a138008b91f8a9d20108
dannymlevine <danlevine@Dans-MacBook-Pro.local>
2014-12-07 04:04:09 -0800

    fixed spacing issues
    cleaned up code made it more readable

--- widget-from-scratch/widget-from-scratch.js
+++ widget-from-scratch/widget-from-scratch.js
@@ -1,15 +1,11 @@
-(function($){
-
-	console.log($("#rangeinput").attr("onchange"))
-
-
-$("#rangeinput").on("change",function(){
-	if($("#newValue").val()>50){
-		$("#gender").val("female")
-	}else if($("#newValue").val()<50){
-		$("#gender").val("male")
-	}else{
-		$("#gender").val("Select Gender")
+(function( $ ) {
+$( "#rangeinput" ).on( "change",function() {
+  if($( "#newValue" ).val()>50) {
+	  $( "#gender" ).val( "female" )
+  }else if($( "#newValue" ).val()<50) {
+	  $( "#gender" ).val( "male" )
+  }else {
+	  $( "#gender" ).val( "Select Gender" )
 	}
 })
-}(jQuery))
+}( jQuery ))
