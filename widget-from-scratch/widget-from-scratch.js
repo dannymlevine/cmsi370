532460af0e46e0a87f018c8fa60a19a3394a5d3d
dannymlevine <danlevine@Dans-MacBook-Pro.local>
2014-12-07 03:39:11 -0800

    Made a widget
    Changed my widget to a slider bar. Its used as a selector for gender in
    my rpg.

--- widget-from-scratch/widget-from-scratch.js
+++ widget-from-scratch/widget-from-scratch.js
@@ -1,1 +1,15 @@
-widget-from-scratch.js
+$(function(){
+
+	console.log($("#rangeinput").attr("onchange"))
+
+
+$("#rangeinput").on("change",function(){
+	if($("#newValue").val()>50){
+		$("#gender").val("female")
+	}else if($("#newValue").val()<50){
+		$("#gender").val("male")
+	}else{
+		$("#gender").val("Select Gender")
+	}
+})
+})
